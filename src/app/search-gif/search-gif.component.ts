07aedd905450179982c257168b8ca693a8d78175
18981a03a2 <18981a03a2@raghuenggcollege.in>
2022-05-19 18:57:27 +0530

    master commit 19-05

--- src/app/search-gif/search-gif.component.ts
+++ src/app/search-gif/search-gif.component.ts
@@ -1,21 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import{GiphyService}from"../services/giphy.service";
+import { NavigationComponent } from '../navigation/navigation.component';
 @Component({
   selector: 'app-search-gif',
   templateUrl: './search-gif.component.html',
   styleUrls: ['./search-gif.component.css']
 })
 export class SearchGifComponent implements OnInit {
-
-  constructor(private gs:GiphyService) { }
-
+constructor(private nc:NavigationComponent,private gs:GiphyService) { }
+gigs:any[]=[]
   ngOnInit(): void {
   }
-  search(query:string){
-    if(query!=""){
-      this.gs.getGifbyparameters(query).subscribe(data=>{
-        console.log(data.data);
-      });
-  }
-}
 }
